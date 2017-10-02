96c9401a156b9c296cce24c1cfa873fa80324d71
BRESSOT ARNAUD p1505990 <p1505990@b710l0104.univ-lyon1.fr>
2017-10-02 08:26:47 +0200

    Base Projet

---
+++ liflf-projet-2017/src/Accept.cpp
@@ -0,0 +1,42 @@
+//******************************************************************************
+
+#include <iostream>
+#include <algorithm>
+
+#include "Accept.h"
+#include "FA_tools.h"
+
+////////////////////////////////////////////////////////////////////////////////
+
+bool EstDeterministe(const sAutoNDE& at){
+  //TODO définir cette fonction
+
+  return true;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+void Fermeture(const sAutoNDE& at, etatset_t& e){
+  //TODO définir cette fonction
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+etatset_t Delta(const sAutoNDE& at, const etatset_t& e, symb_t c){
+  //TODO définir cette fonction
+
+  etatset_t r;
+
+  return r;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+bool Accept(const sAutoNDE& at, std::string str){
+  //TODO définir cette fonction
+
+  return false;
+}
+
+//******************************************************************************
