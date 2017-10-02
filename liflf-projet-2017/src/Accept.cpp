5982eebc4152b684a66460df110c4ac64b13b404
BRESSOT ARNAUD p1505990 <p1505990@b710l0104.univ-lyon1.fr>
2017-10-02 09:26:49 +0200

    Mauvais determinisme

--- liflf-projet-2017/src/Accept.cpp
+++ liflf-projet-2017/src/Accept.cpp
@@ -10,7 +10,31 @@
 
 bool EstDeterministe(const sAutoNDE& at){
   //TODO définir cette fonction
-
+	bool doublon = false;
+	int tab[nb_symbs];
+	
+	for(int i=0;i<trans.size();i++){
+		
+		for(int i=0;i<nb_symbs;i++){ //on vide le tableau
+		tab[i]=0;
+		}
+		
+		for(int j=0;j<trans.at(i).size();j++){
+			
+			if(trans.at(i)==NULL || trans.at(i).size()==0){ //si ne case ne contient pas de vecteur ou un vecteur vide
+				return false;
+			}
+			
+			for(int k=0;i<trans.at(i).at(j).size;k++){
+				
+				if(tab[trans.at(i).at(j).at(k)]!=0){ //si on a un doublon
+					return false;
+				}
+				
+				tab[trans.at(i).at(j).at(k)]++; //Quand on trouve un etat auquel on peut accédé, on l'incremente dans le tableau
+			}
+		}
+	}
   return true;
 }
 
