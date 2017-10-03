52e3607e14780bc18e26881e7b8d5335241dc183
Thrawnchiss <cyril_reymond@hotmail.fr>
2017-10-03 21:50:36 +0200

    Accepte OK, pas teste

--- liflf-projet-2017/src/Accept.cpp
+++ liflf-projet-2017/src/Accept.cpp
@@ -35,8 +35,22 @@ etatset_t Delta(const sAutoNDE& at, const etatset_t& e, symb_t c){
 
 bool Accept(const sAutoNDE& at, std::string str){
   //TODO définir cette fonction
+	if(str.length()= 0){ // si on veut lire le mot vide
+      if(finaux.find(initial)!= finaux.end()){ // si l'état initial est aussi un état final
+        return true; // le mot vide est accepté
+      }
+    }
 
+  for(int i = 0; i < trans.size(); i++){ // parcours des état de l'automate
+    for(int j = 0; j < trans.size(); j++){ // parcours des index de transitions
+      if(trans[i][str.back()][j] != NULL && finaux.find(trans.at(i))!= finaux.end()){
+       // test si la transition existe avec le caractere de la chaine et si état état final
+        return true;
+      }else return false;
+    }
+
+  }
   return false;
-}
+	}
 
 //******************************************************************************
